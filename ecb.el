f1f2db5a00984f685108ef6a2f561d54e01c8c24
Michel Lafon-Puyo <michel.lafonpuyo@gmail.com>
2011-04-15 16:27:57 +0200

    ecb.el: use my locally built ecb version

--- ecb.el
+++ ecb.el
@@ -3,16 +3,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;
 
 (add-to-list 'load-path
-                 "/usr/share/emacs/site-lisp/ecb")
+                 "~/emacs/ecb")
 
 (when (require 'ecb nil t)
 ;;(require 'ecb-autoloads)
 
-(setq ecb-options-version "2.40")
+;;(setq ecb-options-version "2.40")
 (setq ecb-auto-activate nil)
 (setq ecb-compile-window-height 5)
 (setq ecb-layout-name "leftright1")
-(setq ecb-source-path (quote ("/usr/share/emacs/site-lisp/ecb")))
+;;(setq ecb-source-path (quote ("~/emacs/ecb")))
 (setq ecb-tip-of-the-day nil)
 
 (if (frame-parameter nil 'display) (set-face-font 'ecb-default-general-face (frame-parameter nil 'font) ))
