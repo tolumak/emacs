dc26f9ed39dafa6080646b16c9a1e82aa1f1a365
Michel Lafon-Puyo <mlafon-puyo@sise.fr>
2011-06-27 09:49:24 +0200

    whitespace.el: whitespace mode is automatically started in C/C++ mode

--- whitespace.el
+++ whitespace.el
@@ -1,5 +1,12 @@
+(defun c-common-whitespace-hook ()
+  (whitespace-mode)
+  )
+
+
+
 (when
   (require 'whitespace nil t)
   (setq whitespace-line-column 80)
   (setq whitespace-style '(face trailing lines-tail empty))
+  (add-hook 'c-mode-common-hook 'c-common-whitespace-hook t)
   )
