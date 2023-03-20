764eb063cfeb35c76dd8a4953e0a91b08224de34
Michel Lafon-Puyo <michel.lafonpuyo@gmail.com>
2023-03-20 12:30:02 +0100

    add conf for latitude laptop

---
+++ latitude.el
@@ -0,0 +1,4 @@
+;;(new-kde-project "kde/train_schedule" "~/train_schedule_plasmoid" "kdelibs")
+;;(new-kde-project "kde/system-monitor" "~/KDE-dev/system-monitor" "kdelibs")
+;;(new-kde-project "qt/cpufreq" "~/cpufreq_plasmoid")
+;;(new-ede-project "flalbum2mail" "~/flalbum2mail" "linux")
